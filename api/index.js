3418f6823b0a6463e6fcf024c3d5abd9bb141ee0
socian <socian@socians-MBP.lan>
2023-09-11 21:50:18 +0800

    test vercel

--- api/index.js
+++ api/index.js
@@ -1,27 +1,62 @@
-const express = require('express')
+const app = require('express')();
+const compression = require('compression');
+const serveStatic = require('serve-static');
+const bodyParser = require('body-parser');
+const path = require('path');
+const user = require('../server/user');
+const github = require('../server/github');
+const pdf = require('../server/pdf');
+const pandoc = require('../server/pandoc');
+const conf = require('../server/conf');
 
-const app = express()
-const env = require('../config/prod.env');
+const resolvePath = pathToResolve => path.join(__dirname, '..', pathToResolve);
 
-Object.keys(env).forEach((key) => {
-  if (!process.env[key]) {
-    process.env[key] = JSON.parse(env[key]);
-  }
+// Enable CORS for fonts
+app.all('*', (req, res, next) => {
+    if (/\.(eot|ttf|woff2?|svg)$/.test(req.url)) {
+    res.header('Access-Control-Allow-Origin', '*');
+    }
+    next();
 });
 
-const http = require('http');
+// Use gzip compression
+app.use(compression());
 
-require('../server')(app);
+app.get('/oauth2/githubToken', github.githubToken);
+app.get('/conf', (req, res) => res.send(conf.publicValues));
+app.get('/userInfo', user.userInfo);
+app.post('/pdfExport', pdf.generate);
+app.post('/pandocExport', pandoc.generate);
+app.post('/paypalIpn', bodyParser.urlencoded({
+extended: false,
+}), user.paypalIpn);
 
-const port = parseInt(process.env.PORT || 8080, 10);
-const httpServer = http.createServer(app);
-httpServer.listen(port, null, () => {
-  console.log(`HTTP server started: http://localhost:${port}`);
-});
+// Serve landing.html
+app.get('/', (req, res) => res.sendFile(resolvePath('static/landing/index.html')));
+// Serve sitemap.xml
+app.get('/sitemap.xml', (req, res) => res.sendFile(resolvePath('static/sitemap.xml')));
+// Serve callback.html
+app.get('/oauth2/callback', (req, res) => res.sendFile(resolvePath('static/oauth2/callback.html')));
+// Google Drive action receiver
+app.get('/googleDriveAction', (req, res) =>
+res.redirect(`./app#providerId=googleDrive&state=${encodeURIComponent(req.query.state)}`));
 
-// Handle graceful shutdown
-process.on('SIGTERM', () => {
-  httpServer.close(() => {
-    process.exit(0);
-  });
-});
+// Serve static resources
+if (process.env.NODE_ENV === 'production') {
+// Serve index.html in /app
+app.get('/app', (req, res) => res.sendFile(resolvePath('dist/index.html')));
+
+// Serve style.css with 1 day max-age
+app.get('/style.css', (req, res) => res.sendFile(resolvePath('dist/style.css'), {
+    maxAge: '1d',
+}));
+
+// Serve the static folder with 1 year max-age
+app.use('/static', serveStatic(resolvePath('dist/static'), {
+    maxAge: '1y',
+}));
+
+app.use(serveStatic(resolvePath('dist')));
+}
+
+module.exports = app
