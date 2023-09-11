64e6d78a03950449f6bd2c5b77a6290e38ac81b2
socian <socian@socians-MBP.lan>
2023-09-11 22:04:02 +0800

    test vercel

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const pdf = require('./pdf');
 const pandoc = require('./pandoc');
 const conf = require('./conf');
 
-const resolvePath = pathToResolve => path.join(__dirname, '..', pathToResolve);
+const resolvePath = pathToResolve => path.join(__dirname, './dist', pathToResolve);
 
 module.exports = (app) => {
   if (process.env.NODE_ENV === 'production') {
